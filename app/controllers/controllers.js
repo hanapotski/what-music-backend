5d0112dac05d95a1e9b34260a2f68c131edb9348
johnahnz0rs <johnahn123@gmail.com>
2018-09-18 00:51:25 -0700

    redirect uri

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -45,7 +45,7 @@ module.exports = {
 
         request.post(authOptions, function(error, response, body) {
             let access_token = body.access_token;
-            let uri = process.env.FRONTEND_URI/user;
+            let uri = process.env.FRONTEND_URI + '/user';
             res.redirect(uri + '?access_token=' + access_token);
         });
     },
